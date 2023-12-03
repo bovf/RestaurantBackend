611c26137292acbe0427e6c0d22f7470abe2dc99
Dobry <dobry989@gmail.com>
2023-12-03 15:17:18 +0200

    Adding gin routing to the project

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package RestaurantBeckend
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"os"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
+	router := gin.New()
+	router.Use(gin.Logger())
+}
