98f060a6ce3c40ea92069aa53df5ae7c8bcc9d19
Nikolov, Dobry <dobry.nikolov.sp@lht.dlh.de>
2024-01-31 16:34:31 +0100

    Add controllers skeleton

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,1 +1,24 @@
-package controllers
+package controller
+
+import "github.com/gin-gonic/gin"
+
+
+func GetFoods() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func GetFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func CreateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
+
+func UpdateFood() gin.HandlerFunc {
+	return func(c *gin.Context) {
+	}
+}
